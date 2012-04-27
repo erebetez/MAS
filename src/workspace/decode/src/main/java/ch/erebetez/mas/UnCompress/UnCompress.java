a5df54f46e292697baa2493c6028e8e5041ba3a5
Etienen Rebetez <etienne.rebetez@cslbehring.com>
2012-04-27 10:07:12 +0200

    add License and licence text to project.

--- src/workspace/decode/src/main/java/ch/erebetez/mas/UnCompress/UnCompress.java
+++ src/workspace/decode/src/main/java/ch/erebetez/mas/UnCompress/UnCompress.java
@@ -1,3 +1,23 @@
+/* *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * 
+    Library to decode, encode in different Formats, like gzip, zlib.
+    Input and output is as base64 String
+    Copyright (C) 2012  Etienne Rebetez
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  */
+
+
 package ch.erebetez.mas.UnCompress;
 
 public interface UnCompress {
