3d8f86af6876b65d98faa879a56bba7c1e02d060
Etienne Rebetez <etienne.rebetez@oberwallis.ch>
2012-04-29 21:36:10 +0200

    try out gradle. Looks pretty good. basic archetype generation with maven...

---
+++ src/workspace/gradledemo/src/main/java/ch/erebetez/gradletest/App.java
@@ -0,0 +1,13 @@
+package ch.erebetez.gradletest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
