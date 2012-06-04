935bba5f3891f13bc6ddf442ac29e73522469cfd
Etienne Rebetez <etienne.rebetez@oberwallis.ch>
2012-06-04 16:25:24 +0200

    pimp the reviewtask with a table that shows all subprocesses and the form propertys.

--- src/workspace/ActivitiTestApp4/src/main/java/ch/erebetez/activititestapp4/bpmn/forms/CheckInventoryReportForm.java
+++ src/workspace/ActivitiTestApp4/src/main/java/ch/erebetez/activititestapp4/bpmn/forms/CheckInventoryReportForm.java
@@ -2,14 +2,20 @@ package ch.erebetez.activititestapp4.bpmn.forms;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 import org.activiti.engine.history.HistoricDetail;
+import org.activiti.engine.history.HistoricFormProperty;
 import org.activiti.engine.history.HistoricProcessInstance;
+import org.activiti.engine.history.HistoricTaskInstance;
+import org.activiti.engine.task.Task;
 
 import ch.erebetez.activititestapp4.dataobjects.InventoryItem;
 import ch.erebetez.activititestapp4.ui.util.AbstractUserTaskForm;
 
+import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Table;
 import com.vaadin.ui.TextField;
 
 public class CheckInventoryReportForm extends AbstractUserTaskForm {
@@ -18,13 +24,42 @@ public class CheckInventoryReportForm extends AbstractUserTaskForm {
 
 	public static final String FORM_KEY = "checkInventoryResultsForm";
 
+	private Table resultTable;
+	
 	private TextField comments;
-	
-	private class invetoryTaskDao {
-		
-		
+
+	private List<InvetoryTaskDao> invetoryTaskList = new Vector<InvetoryTaskDao>();
+
+	public class InvetoryTaskDao {
+		private String name;
+		private String ok;
+		private String oos;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getOk() {
+			return ok;
+		}
+
+		public void setOk(String ok) {
+			this.ok = ok;
+		}
+
+		public String getOos() {
+			return oos;
+		}
+
+		public void setOos(String oos) {
+			this.oos = oos;
+		}
+
 	}
-	
 
 	@Override
 	public String getDisplayName() {
@@ -40,12 +75,49 @@ public class CheckInventoryReportForm extends AbstractUserTaskForm {
 	protected void populateFormInit(String taskId, String executionId) {
 		// TODO show some actuall data from the subprocesses.
 
-		List<HistoricProcessInstance> subHist = getHistoryService().createHistoricProcessInstanceQuery().superProcessInstanceId(getTask().getProcessInstanceId()).list();
-		
-		for(HistoricProcessInstance subInst: subHist){
-			List<HistoricDetail> detailHist = getHistoryService().createHistoricDetailQuery().processInstanceId(subInst.getId()).list();
+		List<HistoricProcessInstance> subHist = getHistoryService()
+				.createHistoricProcessInstanceQuery()
+				.superProcessInstanceId(getTask().getProcessInstanceId())
+				.list();
+
+		for (HistoricProcessInstance subInstance : subHist) {
+			List<HistoricDetail> detailHist = getHistoryService()
+					.createHistoricDetailQuery().formProperties()
+					.processInstanceId(subInstance.getId())
+					.orderByVariableName().asc()
+					.list();
+
+			List<HistoricTaskInstance> taskHist = getHistoryService()
+					.createHistoricTaskInstanceQuery()
+					.processInstanceId(subInstance.getId()).list();
+
+			// FIXME check list length
+			InvetoryTaskDao inventoryTask = new InvetoryTaskDao();
+			inventoryTask.setName(taskHist.get(0).getName());
+
+			for (int i = 0; i < detailHist.size(); ++i) {
+				HistoricFormProperty detailItem = (HistoricFormProperty) detailHist.get(i);			
+				
+				if(detailItem.getPropertyId().equals("isItemOk")){
+				    inventoryTask.setOk(detailItem.getPropertyValue().toString());
+				} 
+
+				if (detailItem.getPropertyId().equals("oosNumber")){
+					inventoryTask.setOos(detailItem.getPropertyValue().toString());
+				}
+				
+			}
 			
+			invetoryTaskList.add(inventoryTask);
 		}
+		
+
+		BeanItemContainer<InvetoryTaskDao> dataSource = new BeanItemContainer<InvetoryTaskDao>(
+				InvetoryTaskDao.class, invetoryTaskList);
+
+		resultTable = new Table();
+		resultTable.setContainerDataSource(dataSource);
+		addComponent(resultTable);
 
 	}
 
